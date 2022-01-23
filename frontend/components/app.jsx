38a1a499fac28b1cc8f95fba7607762902a6ad69
Jerry Phan <jerryphan1@gmail.com>
2022-01-22 16:41:27 -0800

    finished left icons

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import LeftIcons from "./left_icons";
 import TopNavbar from "./top_navbar";
 import LeftNavbar from "./left_navbar";
 import { Route } from "react-router-dom";
@@ -14,6 +15,7 @@ const App = () => (
       {/* <GreetingContainer /> */}
       <TopNavbar/>
       <LeftNavbar/>
+      <LeftIcons/>
     </header>
 
     <AuthRoute path="/login" component={LoginFormContainer} />
