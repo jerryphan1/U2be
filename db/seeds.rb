48a798b0be1c0166f55d5b3ebff041e53805d471
Jerry Phan <jerryphan1@gmail.com>
2022-01-25 10:35:53 -0800

    videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 require 'open-uri'
 
 Video.destroy_all
-v2 = Video.create(title: 'better test', views: 500, user_id: 4)
-v2_thumb = open("https://u2be-dev.s3.us-west-1.amazonaws.com/test_picture.jpg")
+v2 = Video.create(title: 'better test', views: 500, user_id: 1)
+v2_thumb = open("https://u2be-seeds.s3.us-west-1.amazonaws.com/test_picture.jpg")
 v2.thumbnail.attach(io: v2_thumb, filename: 'better_test')
-v2_vid = open('https://u2be-dev.s3.us-west-1.amazonaws.com/Easy_ace_for_tenz.mp4')
+v2_vid = open('https://u2be-seeds.s3.us-west-1.amazonaws.com/Easy_ace_for_tenz.mp4')
 v2.uploaded_video.attach(io: v2_vid, filename: 'better_vid')
 v2.save
 
-v3 = Video.create(title: 'better test', views: 400, user_id: 4)
-v3_thumb = open("https://u2be-dev.s3.us-west-1.amazonaws.com/test_picture.jpg")
+v3 = Video.create(title: 'better test', views: 400, user_id: 1)
+v3_thumb = open("https://u2be-seeds.s3.us-west-1.amazonaws.com/test_picture.jpg")
 v3.thumbnail.attach(io: v3_thumb, filename: 'better_test')
-v3_vid = open('https://u2be-dev.s3.us-west-1.amazonaws.com/Easy_ace_for_tenz.mp4')
+v3_vid = open('https://u2be-seeds.s3.us-west-1.amazonaws.com/Easy_ace_for_tenz.mp4')
 v3.uploaded_video.attach(io: v3_vid, filename: 'better_vid')
 v3.save
