3e7e1263410b42ffaa32dd95ed1861f24a5fbb00
Jerry Phan <jerryphan1@gmail.com>
2022-01-25 17:42:23 -0800

    too many show divs

--- frontend/components/videos/video_show.jsx
+++ frontend/components/videos/video_show.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import LeftNavbar from "../left_navbar";
+import TopNavbar from "../top_navbar";
 
 export default class VideoShow extends React.Component {
   constructor(props){
@@ -22,11 +25,48 @@ export default class VideoShow extends React.Component {
   // this.props.video?.uploaded_video  "" checks for value if not itll return empty, 
   // good to run with the id because refreshes can take time
   render(){
-    return (
-      <div id='video-show-container'>
-        <video id='video-show-video' src={this.props.video?.uploaded_video || ""} controls/>
 
-      </div>
+    return (
+
+      <div>
+        <TopNavbar/>
+        <LeftNavbar/>
+            <video id='video-show-video' src={this.props.video?.uploaded_video || ""} controls/>
+          <div id='video-show-container'>
+            <div id ='video-show-info'>
+                <div id='video-show-top-info-container'>
+                    <h1>{this.props.video?.title || ""}</h1>
+                      <div className="video-show-format-container">
+                        <div id='video-show-views-create'>
+                          <h3 className="video-show-views">{this.props.video?.views || ""} views</h3>
+                          <h3>{this.props.video?.createdAt || ""}</h3>
+                        </div>
+                        <div id='video-show-likes-dislikes'>
+                          <p> 9 likes</p>
+                          <p>6 dislikes</p>
+                        </div>
+                      </div>
+                </div>
+                <div id='video-show-image-username'>
+                    <Link to='/' className='video-show-image'><p>J</p></Link>
+                    <div>
+                      <h2>{(this.props.video?.user.username[0].toUpperCase() + 
+                          this.props.video?.user.username.slice(1).toLowerCase())
+                            || ""}</h2>
+                      <h3>Description</h3>
+                    </div>
+
+                </div>
+            </div>
+
+            <div id='video-show-suggested-container'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugit rem. Architecto ex iure modi dignissimos labore incidunt voluptatibus ea iste dolore quaerat. Fuga, mollitia vitae in itaque tempora tenetur.</p>  
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugit rem. Architecto ex iure modi dignissimos labore incidunt voluptatibus ea iste dolore quaerat. Fuga, mollitia vitae in itaque tempora tenetur.</p>  
+              
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugit rem. Architecto ex iure modi dignissimos labore incidunt voluptatibus ea iste dolore quaerat. Fuga, mollitia vitae in itaque tempora tenetur.</p>  
+            </div>
+          </div>
+      </div>   
     )
   }
 }
