be3786948bc4f57f460ae9a794cbafdef74323ef
Jerry Phan <jerryphan1@gmail.com>
2022-01-27 15:28:44 -0800

    quick bug

--- frontend/reducers/likes_errors_reducer.js
+++ frontend/reducers/likes_errors_reducer.js
@@ -3,9 +3,9 @@ import { RECEIVE_LIKE_ERRORS, RECEIVE_LIKES } from "../actions/like_actions";
 const LikesErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_COMMENT_ERRORS:
+    case RECEIVE_LIKE_ERRORS:
       return action.errors;
-    case RECEIVE_COMMENTS:
+    case RECEIVE_LIKES:
       return [];
     default:
       return state;
